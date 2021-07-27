44cadbdc5357163bc5243a4c2807753d0ca92648
SmartDragon1225 <2455404279@qq.com>
2021-07-27 10:47:56 +0800

    增加了一个快速排序的方法！

--- springboot-03-web/src/main/java/com/tian/dev/Dev.java
+++ springboot-03-web/src/main/java/com/tian/dev/Dev.java
@@ -1,5 +1,8 @@
 package com.tian.dev;
 
+import java.sql.Array;
+import java.util.Arrays;
+
 /**
  * Package: com.wu.dev
  * Description： TODO
@@ -12,5 +15,42 @@ package com.tian.dev;
 public class Dev {
     public static void main(String[] args) {
         System.out.println("这是测试多人开发的git提交代码！");
+        int arr[] = {0,5,4,3,2,7,8};
+        quick(arr,0,arr.length-1);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void quick(int arr[],int left,int right){
+        int l = left;
+        int r = right;
+        int q = arr[(left+right)/2];
+        while (l<r){
+            while (arr[l]<q){
+                l++;
+            }
+            while (arr[r]>q){
+                r--;
+            }
+            if(l>=r){
+                break;
+            }
+            int t = arr[l];arr[l] = arr[r];arr[r] = t;
+            if(arr[l]==q){
+                r--;
+            }
+            if(arr[r]==q){
+                l++;
+            }
+        }
+        if(l==r){
+            l++;
+            r--;
+        }
+        if(left<r){
+            quick(arr,left,r);
+        }
+        if(right>l){
+            quick(arr,l,right);
+        }
     }
 }
