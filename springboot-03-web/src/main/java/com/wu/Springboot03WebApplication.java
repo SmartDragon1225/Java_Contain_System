956c78ae727b66e8f302f545d74d41ab585322f2
SmartDragon1225 <2455404279@qq.com>
2021-07-27 10:22:25 +0800

    Initial commit！

---
+++ springboot-03-web/src/main/java/com/wu/Springboot03WebApplication.java
@@ -0,0 +1,14 @@
+package com.wu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class Springboot03WebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot03WebApplication.class, args);
+    }
+
+}
