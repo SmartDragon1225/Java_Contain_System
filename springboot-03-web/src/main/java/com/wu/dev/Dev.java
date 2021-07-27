f3a0d72515bf5a6f031f4bbdae98b3e3a08e4b69
SmartDragon1225 <2455404279@qq.com>
2021-07-27 10:35:48 +0800

    添加了一个Dev包

---
+++ springboot-03-web/src/main/java/com/wu/dev/Dev.java
@@ -0,0 +1,16 @@
+package com.wu.dev;
+
+/**
+ * Package: com.wu.dev
+ * Description： TODO
+ * Author: 田智龙
+ * Date: Created in 2021/7/27 10:25
+ * Company: 山东理工大学
+ * Copyright: Copyright (c) 2021
+ * Modified By: SmartDragon
+ */
+public class Dev {
+    public static void main(String[] args) {
+        System.out.println("这是测试多人开发的git提交代码！");
+    }
+}
